64db05522884c00b52e082ba4cb40f6383318b0e
Connor Burton <connorjburton@gmail.com>
2018-12-01 15:42:50 +0000

    Part two of day one done

--- day-1/index.js
+++ day-1/index.js
@@ -2,10 +2,42 @@ const fsConstants = require('fs').constants;
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const index = async function index() {
+const getContents = async function getContents() {
     const contents = await fsPromises.readFile(path.resolve(__dirname, 'data'), { encoding: 'UTF-8', flag: fsConstants.O_RDONLY });
-    const formatted = contents.split('\n').filter(value => value.trim()).map(value => parseInt(value.trim()));
+    return contents.split('\n').filter(value => value.trim()).map(value => parseInt(value.trim()));
+}
+
+const partOne = async function partOne() {
+    const formatted = await getContents();
     return formatted.reduce((prev, current) => prev + current);
 }
 
-module.exports = index;
+const partTwo = async function partTwo() {
+    const formatted = await getContents();
+    let i = 0;
+    let total = 0;
+    const seen = [0];
+    
+    const length = formatted.length;
+    while (true) {
+        for (i; i < length; i++) {
+            total = total + formatted[i];
+            
+            if (seen.includes(total)) {
+                return total;
+            }
+
+            seen.push(total);
+
+            if (i === length - 1) {
+                i = -1;
+                continue;
+            }
+        }
+    }
+}
+
+module.exports = {
+    partOne,
+    partTwo
+};
--- day-1/index.test.js
+++ day-1/index.test.js
@@ -1,34 +1,83 @@
 const fs = require('fs');
-const index = require('./index.js');
+const { partOne, partTwo } = require('./index.js');
 
 describe('day 1', () => {
-    test('positive result works', async () => {
-        const data = `+1
-        +1
-        +1
-        `;
-        fs.promises.readFile = jest.fn(() => data);
+    describe('part one', () => {
+        test('positive result works', async () => {
+            const data = `+1
+            +1
+            +1
+            `;
+            fs.promises.readFile = jest.fn(() => data);
 
-        await expect(index()).resolves.toBe(3);
+            await expect(partOne()).resolves.toBe(3);
+        });
+
+        test('zero result works', async () => {
+            const data = `+1
+            +1
+            -2
+            `;
+            fs.promises.readFile = jest.fn(() => data);
+
+            await expect(partOne()).resolves.toBe(0);
+        });
+
+        test('negative result works', async () => {
+            const data = `-1
+            -2
+            -3
+            `;
+            fs.promises.readFile = jest.fn(() => data);
+
+            await expect(partOne()).resolves.toBe(-6);
+        });
     });
 
-    test('zero result works', async () => {
-        const data = `+1
-        +1
-        -2
-        `;
-        fs.promises.readFile = jest.fn(() => data);
+    describe('part two', () => {
+        test('0 first reach', async () => {
+            const data = `+1
+            -1
+            `;
+            fs.promises.readFile = jest.fn(() => data);
 
-        await expect(index()).resolves.toBe(0);
-    });
+            await expect(partTwo()).resolves.toBe(0);
+        });
 
-    test('negative result works', async () => {
-        const data = `-1
-        -2
-        -3
-        `;
-        fs.promises.readFile = jest.fn(() => data);
+        test('10 first reach', async () => {
+            const data = `+3
+            +3
+            +4
+            -2
+            -4
+            `;
+            fs.promises.readFile = jest.fn(() => data);
 
-        await expect(index()).resolves.toBe(-6);
+            await expect(partTwo()).resolves.toBe(10);
+        });
+
+        test('5 first reach', async () => {
+            const data = `-6
+            +3
+            +8
+            +5
+            -6
+            `;
+            fs.promises.readFile = jest.fn(() => data);
+
+            await expect(partTwo()).resolves.toBe(5);
+        });
+
+        test('14 first reach', async () => {
+            const data = `+7
+            +7
+            -2
+            -7
+            -4
+            `;
+            fs.promises.readFile = jest.fn(() => data);
+
+            await expect(partTwo()).resolves.toBe(14);
+        });
     });
 });
